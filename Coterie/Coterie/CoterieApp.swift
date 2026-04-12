a8c7868392ce8d5aac43454bc2be931cf9569494
Karina <kkarinatam@gmail.com>
2026-04-12 09:52:18 +0100

    fix: remove nested git repo, add Coterie as regular folder

---
+++ Coterie/Coterie/CoterieApp.swift
@@ -0,0 +1,32 @@
+//
+//  CoterieApp.swift
+//  Coterie
+//
+//  Created by Karina Tam on 4/3/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CoterieApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
